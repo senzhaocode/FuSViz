c4db908c85c964e1f1dbb648b3ce56cb19091fa6
Sen ZHAO <t.cytotoxic@gmail.com>
2024-05-01 12:58:05 +0200

    Add files via upload

--- R/Word_process.R
+++ R/Word_process.R
@@ -5,6 +5,7 @@
 #' @param word A data.frame object with two columns (i.e. \code{'name'} - sample; \code{'gene'} - symbol).
 #' @param gene_freq A numeric value for data filtering using recurrent freq of partner genes.
 #' @param type A character string (e.g. 'RNA', 'DNA' or 'Mut').
+#' @param cancergenes A list of cancer genes (e.g. \code{'oncogene'}, \code{'tumorsuppress'} or \code{'related'}).
 #' @param onco_color A character string represents color code for oncogenes (default: '#ff8566').
 #' @param supp_color A character string represents color code for tumor suppressed genes (default: '#00ccff').
 #' @param rela_color A character string represents color code for cancer-related genes (default: '#ffcc33').
@@ -12,7 +13,7 @@
 #' @return A list with two elements (i.e. \code{'freq'} - a data.frame with two columns; \code{'colorlist'} - a vector)
 #'
 #' @export
-wordcloud_processs <- function(word, gene_freq, type, onco_color, supp_color, rela_color) {
+wordcloud_processs <- function(word, gene_freq, type, cancergenes, onco_color, supp_color, rela_color) {
 	stopifnot(is.character(type));
 	stopifnot(is.numeric(gene_freq));
 
@@ -26,11 +27,11 @@ wordcloud_processs <- function(word, gene_freq, type, onco_color, supp_color, re
 	word = unique(word);	word = word$gene;	word = table(word);
 	#// control the frequency
 	word = word[word >= gene_freq];
-	word_onco = word[names(word) %in% names(oncogenes)];
+	word_onco = word[names(word) %in% names(cancergenes$oncogene)];
 	if ( length(word_onco) == 0 ) { word_onco = NULL; }
-	word_supp = word[names(word) %in% names(tumorsupress)];
+	word_supp = word[names(word) %in% names(cancergenes$tumorsuppress)];
 	if ( length(word_supp) == 0 ) { word_supp = NULL; }
-	word_rela = word[names(word) %in% names(related)];
+	word_rela = word[names(word) %in% names(cancergenes$related)];
 	if ( length(word_rela) == 0 ) { word_rela = NULL; }
 	#// if 'word_onco', 'word_supp' and 'word_rela' are NULL
 	if ( is.null(word_onco) && is.null(word_supp) && is.null(word_rela) ) {
